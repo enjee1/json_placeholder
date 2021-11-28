66eee0332d9b6ce0f111fa8d1f46028b02e20c1e
enjee1 <22105805+enjee1@users.noreply.github.com>
2021-11-28 00:12:31 -0500

    Initial commit

---
+++ src/test/java/com/careerdevs/JsonPlaceholder/JsonPlaceholderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.JsonPlaceholder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonPlaceholderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
